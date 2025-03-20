bacf6fe0530e3bab4152eba71c41069dbe33d97e
Stephen Dawson <stephendawson@protonmail.com>
2025-03-20 23:07:00 +0000

    feat: add admin fields

--- os_project/admin.py
+++ os_project/admin.py
@@ -1,20 +1,91 @@
 from django.contrib import admin
 from unfold.admin import ModelAdmin
-# from import_export.admin import ImportExportModelAdmin # ImportExportModel functionality
+from .models import Project, ProjectInterest, ProjectCategory, ProjectTag
 
-# UPDATE THE BELOW IMPORTS WITH THE APP'S MODEL
-# AND UNCOMMENT:
-# from .models import OsProjectModel
 
-# @admin.register(OsProjectModel)
-# class CustomAdminClass(ModelAdmin):
-#     pass
+@admin.register(ProjectCategory)
+class ProjectCategoryAdmin(ModelAdmin):
+    list_display = ("name", "description")
+    search_fields = ("name",)
 
-# -- OR -- THIS WITH IMPORT/EXPORT FUNCTIONALITY:
 
-# class MyAppAdmin(ImportExportModelAdmin):
-#     resource_classes = [MyAppResource]
+@admin.register(ProjectTag)
+class ProjectTagAdmin(ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
 
-# @admin.register(OsProjectModel, MyAppAdmin)
-# class CustomAdminClass(ModelAdmin):
-#     pass
+
+class ProjectInterestInline(admin.TabularInline):
+    model = ProjectInterest
+    extra = 0
+    readonly_fields = ("created_at",)
+    fields = ("user_id", "note", "created_at")
+    # TODO: Update when user profiles are ready
+    # fields = ('wit', 'note', 'created_at')
+
+
+@admin.register(Project)
+class ProjectAdmin(ModelAdmin):
+    list_display = (
+        "title",
+        "status",
+        "difficulty",
+        "owner_id",
+        "assigned_wit_id",
+        "funding_goal",
+        "current_funding",
+        "is_fully_funded",
+        "created_at",
+    )
+    list_filter = ("status", "difficulty", "category", "tags", "created_at")
+    search_fields = ("title", "description", "technologies")
+    readonly_fields = ("created_at", "updated_at")
+    inlines = [ProjectInterestInline]
+
+    fieldsets = (
+        (
+            "Basic Information",
+            {"fields": ("title", "description", "image", "category", "tags")},
+        ),
+        (
+            "Project Details",
+            {
+                "fields": (
+                    "repo_link",
+                    "deploy_link",
+                    "technologies",
+                    "difficulty",
+                    "status",
+                )
+            },
+        ),
+        ("Funding Information", {"fields": ("funding_goal", "current_funding")}),
+        (
+            "Ownership",
+            {
+                "fields": ("owner_id", "assigned_wit_id")
+                # TODO: Update when user profiles are ready
+                # 'fields': ('owner', 'assigned_wit')
+            },
+        ),
+        (
+            "Timestamps",
+            {"fields": ("created_at", "updated_at"), "classes": ("collapse",)},
+        ),
+    )
+
+    def is_fully_funded(self, obj):
+        return obj.is_fully_funded()
+
+    is_fully_funded.boolean = True
+    is_fully_funded.short_description = "Fully Funded"
+
+
+@admin.register(ProjectInterest)
+class ProjectInterestAdmin(ModelAdmin):
+    list_display = ("project", "user_id", "created_at")
+    # TODO: Update when user profiles are ready
+    # list_display = ('project', 'wit', 'created_at')
+    list_filter = ("created_at",)
+    search_fields = ("project__title", "note")
+    readonly_fields = ("created_at",)
