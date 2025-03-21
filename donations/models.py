44ad9bb637a63ca0b56b1b5d657342e0325570f3
Warren Smith <warren.lee.smith@hotmail.co.uk>
2025-03-21 15:20:13 +0000

    UPDATE: Payument model - UUID, user payment details such as name address ETC

--- donations/models.py
+++ donations/models.py
@@ -12,6 +12,15 @@ class Payment(models.Model):
     confirmation_number = models.CharField(max_length=32, null=False, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    country = CountryField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(
         max_length=20,
@@ -22,3 +31,21 @@ class Payment(models.Model):
         max_digits=10, decimal_places=2, null=False, default=Decimal("0.00")
     )
     stripe_payment_intent_id = models.CharField(max_length=254, null=True, blank=True)
+
+    def _generate_confirmation_number(self):
+        """
+        Generate a random, unique confirmation number using UUID.
+        """
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the confirmation number
+        if it hasn't been set already.
+        """
+        if not self.confirmation_number:
+            self.confirmation_number = self._generate_confirmation_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.confirmation_number
