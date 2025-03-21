2707426e1825c5b668a4610db2865649539d1a7e
SoroushGReza <soroush_developer@hotmail.com>
2025-03-21 17:14:50 +0300

    Registered User Profile Types in Admin Panel:
    admin.py:
    - Registered `WomenInTech`, `OS_Maintainer`, and `Mentor` models in
      Django Admin.
    - Added `WomenInTechAdmin` with `list_display` and `search_fields`.
    - Added `OS_MaintainerAdmin` with `github_username` in `list_display`
      and `search_fields`.
    - Added `MentorAdmin` with basic user fields.
    - Ensured `created_at` and `updated_at` fields are displayed for
      tracking.
    
    migrations/0003_mentor.py:
    - Created migrations for `Mentor` model

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -2,10 +2,29 @@
 from django.contrib import admin
 from unfold.admin import ModelAdmin
 from .models import Profile
+from .models import WomenInTech, OS_Maintainer, Mentor
 
 
 @admin.register(Profile)
 class ProfileAdmin(ModelAdmin):
-    list_display = ('user', 'created_at', 'updated_at')
-    search_fields = ('user__username', 'user__email')
-    readonly_fields = ('created_at', 'updated_at')
+    list_display = ("user", "created_at", "updated_at")
+    search_fields = ("user__username", "user__email")
+    readonly_fields = ("created_at", "updated_at")
+
+
+@admin.register(WomenInTech)
+class WomenInTechAdmin(ModelAdmin):
+    list_display = ("user", "created_at", "updated_at")
+    search_fields = ("user__username",)
+
+
+@admin.register(OS_Maintainer)
+class OS_MaintainerAdmin(ModelAdmin):
+    list_display = ("user", "github_username", "created_at", "updated_at")
+    search_fields = ("user__username", "github_username")
+
+
+@admin.register(Mentor)
+class MentorAdmin(ModelAdmin):
+    list_display = ("user", "created_at", "updated_at")
+    search_fields = ("user__username",)
